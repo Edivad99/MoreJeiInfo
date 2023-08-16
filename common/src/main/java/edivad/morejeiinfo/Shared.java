c4c2771b1387aed6d81e487b8c4a1ea93fc3acc9
Davide Albiero <davide2910@live.it>
2023-08-16 11:57:13 +0200

    Fix CD

--- common/src/main/java/edivad/morejeiinfo/Shared.java
+++ common/src/main/java/edivad/morejeiinfo/Shared.java
@@ -7,7 +7,6 @@ import com.mojang.logging.LogUtils;
 public class Shared {
 
   public static final String ID = "morejeiinfo";
-  public static final String NAME = "MoreJeiInfo";
   public static final Logger LOGGER = LogUtils.getLogger();
 
   public static final DecimalFormat DECIMALFORMAT = new DecimalFormat("#.##");
